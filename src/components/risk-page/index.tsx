dcfec1db01e129ad602af91fbbc7d7f18c6af93d
ZloyEgor <elopatov74@outlook.com>
2023-11-26 02:58:32 +0300

    Improve pages style

--- src/components/risk-page/index.tsx
+++ src/components/risk-page/index.tsx
@@ -4,6 +4,12 @@ import { useParams } from 'react-router-dom';
 import { riskServiceMock } from '../../service/risk-service';
 import { RiskTable } from '../risk-form';
 
+import './risk-page.scss';
+import { gigachatServiceMock } from '../../service/gigachat-service';
+import { Col, Row } from 'antd';
+import { ProfileForm } from '../profile-form';
+import { GigachatMessage } from '../gigachat-message';
+
 export const RiskPage: React.FC = () => {
   const { id } = useParams();
 
@@ -12,8 +18,24 @@ export const RiskPage: React.FC = () => {
     queryFn: (id) => riskServiceMock.getRiskInfo(String(id)),
   });
 
+  const { data: riskInfoSummary } = useQuery({
+    queryKey: ['getRiskInfoSummary', id],
+    queryFn: () => gigachatServiceMock.getRiskInfoSummary(String(id)),
+  });
+
   return (
     <div className="sb-risk-page">
-      {riskInfo && (<RiskTable {...riskInfo} />)}
+      <p className="sb-risk-page__title">
+        Расчет уровня риска
+      </p>
+
+      <Row gutter={16}>
+        <Col span={16}>
+          {riskInfo && (<RiskTable {...riskInfo} />)}
+        </Col>
+        <Col span={7}>
+          <GigachatMessage message={riskInfoSummary?.answer || ''}/>
+        </Col>
+      </Row>
     </div>);
 };
