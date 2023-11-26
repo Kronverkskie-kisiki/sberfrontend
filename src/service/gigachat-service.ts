477c3fe37f1ecb443dd20f5e25e1a22e973bcf53
ZloyEgor <elopatov74@outlook.com>
2023-11-26 14:39:24 +0300

    Specify URL for vk-analysis

--- src/service/gigachat-service.ts
+++ src/service/gigachat-service.ts
@@ -60,6 +60,7 @@ export const gigachatServiceMock = {
 export const gigachatService = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   getRiskInfoSummary: (riskInfoId: string) =>
-    fetchServer<GigachatResponse>('https://gigachat.archphoenix.tech/api/loan_rating', 'POST', {}),
-  // fetchServer<GigachatResponse>('https://gigachat.archphoenix.tech/api/health_check', 'GET'),
+    fetchServer<GigachatResponse>('/api/loan_rating', 'POST', {}),
+  getProfileSummary: (userId: string) =>
+    fetchServer<GigachatResponse>('/api/vk-analysis', 'POST', { userId }),
 };
